216212755a2fe555894be3bae3d9207e2c2e028e
Alexander Tretyakov <tpetyakov@gmail.com>
2022-07-14 13:08:08 +0300

    feat: add products service

---
+++ online-store/src/common/products-service.ts
@@ -0,0 +1,32 @@
+import { Product } from "./product";
+import json from '@assets/data-sample.json';
+import { BASE_URL } from "./constants";
+
+export interface IProductsService {
+  load(): Promise<Product[]>
+}
+
+export class DummyProductsService implements IProductsService {
+  public async load(): Promise<Product[]> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const random = Math.random();
+        if (random < 0.1) {
+          return reject(new Error('Data load error!'));
+        }
+        resolve(json);
+      }, 500);
+    });
+  }
+}
+
+export class ProductsService implements IProductsService {
+  public async load(): Promise<Product[]> {
+    const res = await fetch(`${BASE_URL}/data.json`);
+    if (!res.ok) {
+      throw new Error('Data load error!')
+    }
+    const data: Product[] = await res.json();
+    return data;
+  }
+}
--- online-store/src/components/app.ts
+++ online-store/src/components/app.ts
@@ -1,24 +1,31 @@
-import { BASE_URL, CART_PRODUCTS_LIMIT, EVENT } from '@common/constants';
+import { CART_PRODUCTS_LIMIT, EVENT } from '@common/constants';
 import { Component, ComponentProps } from '@core/component';
 import { Filter } from './filters/filter';
-import json from '@assets/data-sample.json';
-import { SORT, sortData, SortingFunction } from '@common/sorting';
+import { SORT, sortData } from '@common/sorting';
 import { isEqualProductsArrays, Product } from '@common/product';
 import { ShopPageView } from '@views/shop-page';
 import { printComponentsTree, selectFrom } from '@common/utils';
-
-const url = `${BASE_URL}/data.json`;
+import { DummyProductsService, IProductsService } from '../common/products-service';
 
 export type ProductsLoadEventData = {
   products: Product[];
   productInCartIds: string[];
+  error: Error | null;
 };
 
+type AppState = {
+  productInCartIds: string[];
+  sort: SORT;
+  filterStates: Record<string, unknown>;
+}
+
 export class App extends Component {
+  private productsService: IProductsService;
   private products: Product[];
   private filtred: Product[];
   private productInCartIds: string[];
-  private sortingFunction: SortingFunction;
+  private sort: SORT;
+  private filterStates: Record<string, unknown>;
   
   constructor(props: ComponentProps, rootSelector: string) {
     super({
@@ -29,31 +36,24 @@ export class App extends Component {
       }
     });
 
+    this.productsService = new DummyProductsService();
     this.products = [];
     this.filtred = [];
-    // this.productInCartIds = [];
-    this.productInCartIds = [ '795abdd5e7673332', '279079561d533332' ];
-
-    this.sortingFunction = sortData[SORT.TITLE_ASC][1];
+    this.productInCartIds = [];
+    // this.productInCartIds = [ '795abdd5e7673332', '279079561d533332' ];
+    this.sort = SORT.TITLE_ASC;
+    this.filterStates = {};
 
     this.on(EVENT.FILTERS_CHANGE, this.handleFiltersChange);
     this.on(EVENT.TRY_ADD_TO_CART, this.handleTryAddToCart);
-    this.on(EVENT.CHANGE_SORT, this.handleChangeSort)
-
-    // fetch(url)
-    //   .then((res): Promise<Product[]> => res.json())
-    //   .then((data: Product[]) => {
-    //     this.handleDataLoad(data);
-    //   })
-    //   .catch((err: Error) => {
-    //     this.handlers?.onDataLoad(this.products);
-    //     super.render(err.message);
-    //   })
-
-    setTimeout(() => {
-      this.handleDataLoad(json);
-      // printComponentsTree.call(this);
-    }, 500);
+    this.on(EVENT.CHANGE_SORT, this.handleChangeSort);
+    
+    this.productsService.load()
+      .then((data) => this.handleDataLoad(data))
+      .catch((err: Error) => {
+        this.emit(EVENT.SHOW_ALERT, `Ошибка при загрузке данных: ${err.message}`);
+        this.handleDataLoad([], err);
+      })
 
     this.getRoot().insertAdjacentHTML('beforeend', this.render());
     this.afterRender();
@@ -61,21 +61,29 @@ export class App extends Component {
   
   private updateProductsList(products: Product[] = this.filtred) {
     const { productInCartIds } = this;
-    this.sortingFunction.call(null, this.filtred);
+    const [_, sortingFunction] = sortData[this.sort];
+    sortingFunction.call(null, this.filtred);
     this.emit(EVENT.PRODUCTS_LIST_UPDATE, { products, productInCartIds });
   }
 
-  private handleDataLoad = (data: Product[]) => {
+  private handleDataLoad = (data: Product[], error: Error | null = null) => {
     this.products = data;
     this.filtred = [...this.products];
-    this.sortingFunction.call(null, this.filtred);
+    const [_, sortingFunction] = sortData[this.sort];
+    sortingFunction.call(null, this.filtred);
 
     const { productInCartIds } = this;
-    this.emit(EVENT.PRODUCTS_LOAD, { products: this.filtred, productInCartIds });
+    this.emit(EVENT.PRODUCTS_LOAD, { products: this.filtred, productInCartIds, error });
   }
 
   private handleFiltersChange = (e: CustomEvent<Filter[]>): void => {
     const filters = e.detail;
+    
+    this.filterStates = filters.reduce((acc: Record<string, unknown>, filter) => {
+      acc[filter.getName()] = filter.getState();
+      return acc;
+    }, {});
+
     const filtred = this.products.filter((item) => {
       return filters.reduce((acc, filter) => (
         acc && filter.check(item)
@@ -92,7 +100,7 @@ export class App extends Component {
     const product = this.products.find((item) => item.id === productId);
     if (product !== undefined && !this.productInCartIds.includes(product.id)) {
       if (this.productInCartIds.length === CART_PRODUCTS_LIMIT) {
-        this.emit(EVENT.SHOW_ALERT, `Cart is overflown! Its limited to ${CART_PRODUCTS_LIMIT} products`);
+        this.emit(EVENT.SHOW_ALERT, `Корзина переполнена! Лимит товаров в корзине: ${CART_PRODUCTS_LIMIT}.`);
       } else {
         this.productInCartIds.push(product.id);
         this.emit(EVENT.ADD_TO_CART, product.id);
@@ -101,7 +109,8 @@ export class App extends Component {
   }
 
   private handleChangeSort = (e: CustomEvent<SORT>) => {
-    this.sortingFunction = sortData[e.detail][1];
+    if (this.sort === e.detail) return;
+    this.sort = e.detail;
     this.updateProductsList();
   }
 }
